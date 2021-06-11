9a0fe87bae654fa1ab96c4c9311808ff5bb33a34
Shivangi Bhagat <shivangibhagat12@gmail.com>
2021-06-10 23:51:13 -0400

    feature/footer

--- custom-index.js
+++ custom-index.js
@@ -18,6 +18,17 @@ jQuery( document ).ready( function( $ ) {
         $this.toggleClass('active').next('ul').toggleClass('active');
     });
 
+    // scroll js for bottom top arrow
+    $(".scroll").click(function(event){  
+        event.preventDefault();  
+        var full_url = this.href;  
+        var parts = full_url.split("#");  
+        var trgt = parts[1];  
+        var target_offset = $("#"+trgt).offset();  
+        var target_top = target_offset.top;  
+        $('html, body').animate({scrollTop:target_top}, 1000);  
+    });  
+
     // sticky menu 
     jQuery(window).scroll(function() {
         var sd = jQuery(window).scrollTop();
